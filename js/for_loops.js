5cf1b93d4148a15b44ae2cafaeea6db5ae5ae2fa
“Javier <you@gmail.com>
2020-06-29 22:01:20 -0500

    workin on 4 and 5

---
+++ js/for_loops.js
@@ -0,0 +1,94 @@
+
+// for (var i = 0, j = 9; i < 10; i++, j--) {
+//     console.log('for loop iteration #' + i + ', j = ' + j);
+// }
+
+// prompt("you reached for_loops.js")
+
+///////////////////showMultiplicationTable #1////////////////////
+//
+// for (let i = 1; i <= 10; i++) {
+//     var a = 7 * i;
+//     // console.log(i);
+//     console.log('7 x '+ i + " = " + a);
+// }
+// console.log("We are done!");
+
+/////////////////////////for Loop #2/////////////////////////////
+// Use a for loop and the code from the previous lessons to
+// generate 10 random numbers between 20 and 200 and output
+// to the console whether each number is odd or even.
+// For example:
+// let a = "Your EVEN number: ";
+// let b = "Your ODD number: ";
+// for (let i = 0; i < 10)
+
+// let evenNumber = "is an even number: "
+// let oddNumber = " is an odd number: "
+//
+// for (var i = 0; i < 10; i++) {
+//     console.log("Round : " + i);
+//     let n =Math.floor(Math.random() * 180) + 20;
+//
+//     if (n % 2 === 0) {
+//         console.log("Your number: " + n + " " + evenNumber);
+//     } else {
+//         console.log("Your number #: " + n + " " + oddNumber);
+//     }
+// }
+// console.log("We are done!");
+///////////////////////for Loop #3////////////////////////////////////
+// Create a for loop that uses console.log to create the
+// output shown below.
+
+// for (let i = 1; i <= 9; i++) {
+//     var a = 7 * i;
+//     // console.log(i);
+//     // console.log(i + " = " + a);
+//     console.log(i);
+// }
+// console.log("We are done!");
+
+///////////////////////for Loop #4////////////////////////////////////
+// Create a for loop that uses console.log to create the
+// output shown below.
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+// for (let i = 1; i <= 9; i++) {
+//     var a = 7 * i;
+//     // console.log(i);
+//     // console.log(i + " = " + a);
+//     console.log(i);
+//     // console.log(a);
+// }
+// console.log("We are done!");
+
+///////////////////////for Loop #5//////////////////////////
+// for (let i = 1; i <= 21; i++) {
+//     var a = 5;
+//     var b = (101 - i);
+//     // console.log(i);
+//     console.log(b);
+//     // console.log('7 x '+ i + " = " + a);
+// }
+// console.log("We are done!");
+
+///////////////////////for Loop #6//////////////////////////
+let n = 5;
+for (let i = 1; i <= 20; i++; ) {
+    var a = 5;
+    // var b = (101 - i);
+    // console.log(i);
+    console.log(a);
+    console.log(i);
+}
+
+console.log("We are done!");
