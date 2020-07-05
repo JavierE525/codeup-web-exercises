6ad1122ec95749e5bc7451ff08bc93afd7084192
“Javier <you@gmail.com>
2020-07-04 21:21:45 -0500

    completed iterating loops

--- js/iterating.js
+++ js/iterating.js
@@ -6,29 +6,56 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    var names1 = ['Alesha', 'Anthony', 'Bear', 'Javier'];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+      console.log(names1);
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log('There are ' + names1.length + ' shapes in the array');
+
+    console.log('The first shape is: ' + names1[0]);
+// The first shape is: square
+
+    console.log('The second shape is: ' + names1[1]);
+// The second shape is: rectangle
+
+    console.log('The third shape is: ' + names1[2]);
+// The third shape is: circle
+
+    console.log('The fourth shape is: ' + names1[3]);
+// The fourth shape is: triangle
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    var names1 = ['square', 'rectangle', 'circle', 'triangle'];
+
+// loop through the array and log the values
+    for (var i = 0; i < names1.length; i++) {
+        console.log('The shape at index ' + i + ' is: ' + names1[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    var names2 = ['square', 'rectangle', 'circle', 'triangle'];
+
+// loop through the array and log the values
+    names2.forEach(function(names2) {
+        console.log('Here is a lovely shape: ' + names2 + '.');
+    });
 
     /**
      * TODO:
@@ -44,4 +71,17 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
 
+    var shapes1 = [1, 2, 3, 4];
+
+    // loop through the array and log the values
+    for (var    i = 0;
+                i < shapes1.length;
+                i++) {
+        console.log(i);
+        console.log
+                ('The number at index [' + i + '] ' + 'is: [' + shapes1[i] + ']');
+    }
+
 })();
+
+
